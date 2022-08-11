a19599566fbe1bfe8c2c346c83ad3c4da992b032
Ben <bencoopernotes@gmail.com>
2022-08-10 22:31:04 -0700

    Horizontal Player Movement

--- BubbleGameJam/Assets/Scripts/PlayerMovement.cs
+++ BubbleGameJam/Assets/Scripts/PlayerMovement.cs
@@ -4,24 +4,86 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float speed = .1f;
-    private void FixedUpdate()
+    [Range(0, 1)]
+    public float speed;
+
+    [Range(0, 50)]
+    public int animFramerate;
+
+    public Sprite tempFacingSprite;
+
+    public Sprite[] animSprite;
+    private SpriteRenderer spriteRenderer;
+
+    private char prevDirection;
+    private int animationStatus;
+
+    private int gameFramesToAnimFrames;
+
+    private void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        animationStatus = 0;
+        prevDirection = 'W';
+    }
+    private void FixedUpdate() //W and S are subject to change
     {
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position = Vector3.Lerp(transform.position, transform.position + Vector3.up, speed); 
+            transform.position = Vector3.Lerp(transform.position, transform.position + Vector3.up, speed);
+            spriteRenderer.sprite = tempFacingSprite;
         }
-        if (Input.GetKey(KeyCode.A))
+
+        else if (Input.GetKey(KeyCode.A))
         {
             transform.position = Vector3.Lerp(transform.position, transform.position + Vector3.left, speed);
+
+            if (prevDirection != 'A')
+            {
+                transform.localRotation = Quaternion.Euler(0, 180, 0);
+                prevDirection = 'A';
+            }
+
+            HorizontalAnimate();
         }
-        if (Input.GetKey(KeyCode.S))
+
+        else if (Input.GetKey(KeyCode.S))
         {
             transform.position = Vector3.Lerp(transform.position, transform.position + Vector3.down, speed);
+            spriteRenderer.sprite = tempFacingSprite;
         }
-        if (Input.GetKey(KeyCode.D))
+
+        else if (Input.GetKey(KeyCode.D))
         {
             transform.position = Vector3.Lerp(transform.position, transform.position + Vector3.right, speed);
+
+            if (prevDirection != 'D')
+            {
+                transform.localRotation = Quaternion.Euler(0, 0, 0);
+                prevDirection = 'D';
+            }
+
+            HorizontalAnimate();
         }
     }
+
+    void HorizontalAnimate()
+    {
+
+        gameFramesToAnimFrames++;
+
+        if (gameFramesToAnimFrames >= 50/animFramerate)
+        {
+            spriteRenderer.sprite = animSprite[animationStatus];
+            animationStatus++;
+
+            if (animationStatus == 10)
+            {
+                animationStatus = 0;
+            }
+
+            gameFramesToAnimFrames = 0;
+        }
+    }
+
 }
