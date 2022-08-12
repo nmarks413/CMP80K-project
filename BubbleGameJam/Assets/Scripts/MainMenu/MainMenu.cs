6ecf98f140d68979061694e146eda40e9984c5af
Nat <nmarks413@gmail.com>
2022-08-11 17:02:34 -0700

    fucking with slider

--- BubbleGameJam/Assets/Scripts/MainMenu/MainMenu.cs
+++ BubbleGameJam/Assets/Scripts/MainMenu/MainMenu.cs
@@ -7,12 +7,15 @@ public class MainMenu : MonoBehaviour
 {
     private Canvas mainMenu;
     private Canvas optionsMenu;
-
+    private Slider volumeSlider;
+    private AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
+
         mainMenu = GameObject.Find("Main").GetComponent<Canvas>();
         optionsMenu = GameObject.Find("OptionsCanvas").GetComponent<Canvas>();
+        audioSource = GetComponent<AudioSource>();
 
         Button startButton = GameObject.Find("Start").GetComponent<Button>();
         Button optionsButton = GameObject.Find("Options").GetComponent<Button>();
@@ -44,12 +47,27 @@ public class MainMenu : MonoBehaviour
     {
         Button backButton = GameObject.Find("Back").GetComponent<Button>();
 
+        Slider volumeSlider = GameObject.Find("Volume").GetComponent<Slider>();
+
         backButton.onClick.AddListener(returnToMainMenu);
+        volumeSlider.onValueChanged.AddListener(delegate { ChangeVolume(); });
+        volumeSlider.maxValue = 1;
+        volumeSlider.minValue = 0;
     }
 
     void returnToMainMenu()
     {
         mainMenu.gameObject.SetActive(true);
         optionsMenu.gameObject.SetActive(false);
+    }
+
+    void ChangeVolume()
+    {
+        Debug.Log(volumeSlider.value);
+        audioSource.volume = volumeSlider.value;
+        Text VolumeText = GetComponent<Text>();
+        VolumeText.text = audioSource.volume.ToString();
+        Debug.Log(audioSource.volume);
+
     }
 }
