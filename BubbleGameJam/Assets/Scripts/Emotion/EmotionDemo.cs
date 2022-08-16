8589c6fb67330191d2582fe7fcca0ddbd06c9f10
nullptr3 <bencoopernotes@gmail.com>
2022-08-16 15:13:13 -0700

    update

--- BubbleGameJam/Assets/Scripts/Emotion/EmotionDemo.cs
+++ BubbleGameJam/Assets/Scripts/Emotion/EmotionDemo.cs
@@ -7,9 +7,8 @@ public class EmotionDemo : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GameObject.Find("Player").GetComponent<SpriteRenderer>().color = Color.black;
+        GameObject.Find("Player").GetComponent<SpriteRenderer>().color = Color.yellow;
     }
-
     // Update is called once per frame
     void Update()
     {
