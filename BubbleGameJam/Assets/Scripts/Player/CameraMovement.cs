bea0b94113f4fdf3d9c6b0e123ffdf343aea9d12
nullptr3 <bencoopernotes@gmail.com>
2022-08-16 15:31:56 -0700

    finalDemo

--- BubbleGameJam/Assets/Scripts/Player/CameraMovement.cs
+++ BubbleGameJam/Assets/Scripts/Player/CameraMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CameraMovement : MonoBehaviour
 {
@@ -9,7 +10,7 @@ public class CameraMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (transform.position.x > -3 && transform.position.x < 31)
+        if (transform.position.x > -3 && transform.position.x < 31 || SceneManager.GetActiveScene().name != "demoScene")
         {
 
             if (transform.position.x - Camera.main.transform.position.x > threshhold)
@@ -21,7 +22,7 @@ public class CameraMovement : MonoBehaviour
                 Camera.main.transform.position = new Vector3(Camera.main.transform.position.x - threshhold, Camera.main.transform.position.y, Camera.main.transform.position.z);
             }
         }
-        if (transform.position.y < -1 && transform.position.y > -3)
+        if (transform.position.y < -1 && transform.position.y > -3 || SceneManager.GetActiveScene().name != "demoScene")
         {
             if (transform.position.y - Camera.main.transform.position.y > threshhold)
             {
